ea2d8e9d8596bf72b525dacc8fca52b1636be46c
lenovo <lenovo@DESKTOP-3INHJVQ>
2026-02-04 03:27:34 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://akas; .ms/new-console-template for more information
+using System.Collections;
+ArrayList sehirler =new ArrayList ();
+sehirler.Add("istanbul");
+sehirler.Add("2");
+foreach(object i in sehirler)
+{
+    Console.WriteLine(i);
+}
